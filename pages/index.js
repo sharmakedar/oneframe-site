c6166d0de10796827b36f98e1ea8ec0f4aded0fb
sharmakedar <kedarsharma44496@gmail.com>
2025-06-26 16:30:45 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', background: 'white', color: 'black', fontFamily: 'sans-serif' }}>
+      <h1>Kedar Sharma</h1>
+      <p>This will be your editable portfolio with CMS soon.</p>
+    </div>
+  );
+}
